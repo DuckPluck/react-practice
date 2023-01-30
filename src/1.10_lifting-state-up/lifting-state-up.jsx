bd778133a6d39420786e65f74a4045af84f31d03
Даниил Уткин <dan4ezzz@gmail.com>
2023-01-30 22:50:27 +0300

    4.2 completed

--- src/1.10_lifting-state-up/lifting-state-up.jsx
+++ src/1.10_lifting-state-up/lifting-state-up.jsx
@@ -4,14 +4,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../styles.scss';
-import {number} from 'prop-types';
+import {number, string, func} from 'prop-types';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
+// Состояния должны храниться в общем родительском компоненте
+const scaleNames = {
+  c: 'Цельсия',
+  f: 'Фаренгейта'
+};
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
 
 export function BoilingVerdict(props) {
-  BoilingVerdict.propTypes = {celsius: number}
+  BoilingVerdict.propTypes = {celsius: number};
 
   if (props.celsius >= 100) {
     return <p>Вода закипит</p>;
@@ -19,28 +42,57 @@ export function BoilingVerdict(props) {
   return <p>Вода НЕ закипит</p>;
 }
 
+export class TemperatureInput extends React.Component {
+  static propTypes = {scale: string, temperature: string, onTemperatureChange: func};
 
-
-export class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {temperature: ''}
+    this.state = {temperature: ''};
   }
 
   handleChange = (e) => {
-    this.setState({temperature: e.target.value});
-  }
+    this.props.onTemperatureChange(e.target.value);
+  };
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
     return (
         <fieldset>
-          <legend>Введите температуру в градусах Цельсия:</legend>
-          <input value={temperature} onChange={this.handleChange} />
-          <BoilingVerdict celsius={parseFloat(temperature)} />
+          <legend>Введите температуру в градусах {scaleNames[scale]}:</legend>
+          <input value={temperature} onChange={this.handleChange}/>
         </fieldset>
     );
   }
 }
 
-root.render(<Calculator />);
+export class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {temperature: '', scale: 'c'};
+  }
+
+  handleCelsiusChange = (temperature) => {
+    this.setState({scale: 'c', temperature});
+  }
+
+  handleFahrenheitChange = (temperature) => {
+    this.setState({scale: 'f', temperature});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+    return (
+        <div>
+          <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange}/>
+          <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange}/>
+          <BoilingVerdict celsius={parseFloat(celsius)}/>
+        </div>
+    );
+  }
+}
+
+root.render(<Calculator/>);
---
+++ src/4.2_hooks-overview/hooks-overview.jsx
@@ -0,0 +1,95 @@
+// https://ru.reactjs.org/docs/hooks-overview.html
+
+import {useState, useEffect, useContext, useReducer} from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../styles.scss';
+
+
+// Хуки не работают внутри классов
+// Хуки нельзя вызывать внутри циклов, условий и вложенных функций
+// также хуки стоит вызывать только в функциональных компонентах React
+
+
+// функция `useState(default)` это хук, который позволяет создать одно поле состояния и метод для его изменения(аналог `.useState()` для одного компонента).
+// Метод принимает аргумент - начальное состояние.
+// В качестве значения можно указать объект.
+function ExampleWithManyStates(props) {
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState({text: 'learn hooks'});
+  // ...
+}
+
+
+
+// `useEffect(callback)` это хук, который выполняет функционал `componentDidMount()`, `componentDidUpdate()` и `componentWillUnmount()`.
+// Функция в колбэке запускается после каждого рендера.
+// Функция в ретерне колбека запускается перед сбросом компонента
+function Example() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times!`;
+    return () => {
+      // логика запустится перед сбросом
+    };
+  });
+
+  return (
+      <div>
+        <p>You clicked {count} times!</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me!
+        </button>
+      </div>
+  );
+}
+
+
+
+// `useContext()` позволяет подписаться на контекст без каких либо вложений.
+function Example2() {
+  const locale = useContext(LocaleContext);
+  const theme = useContext(ThemeContext);
+}
+
+
+
+// `useReducer()` позволяет управлять внутренним состоянием более сложного компонента с помощью редусера
+function Example3() {
+  const [todos, dispatch] = useReducer(todosReducer);
+}
+
+
+
+// Для копирования состояния и логики хуков - работа с формами, анимациями, декларативные подписки, таймеры и тд, можно объединить эти хуки в один - пользовательский хук
+// Имена хуков должны начинаться с 'use...'.
+// Можно использовать один и тот-же хук несколько раз - они имеют независимые состояния
+function useFriendStatus(friendId) {
+  const [isOnline, setIsOnline] = useState(null);
+
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline);
+  }
+
+  useEffect(() => {
+    ChatAPI.subscribeToFriendStatus(friendId, handleStatusChange);
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(friendId, handleStatusChange);
+
+    };
+  });
+
+  return isOnline;
+}
+
+
+function FriendStatus(props) {
+  const isOnline = useFriendStatus(props.friend.id);
+
+  if (isOnline === null) {
+    return 'Loading...';
+  }
+  return isOnline ? 'online' : 'offline';
+}
